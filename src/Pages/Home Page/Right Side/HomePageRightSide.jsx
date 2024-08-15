bb5bd408a6724c928d6984ef0a66fb8b408edec3
faz4563 <79906416+faz4563@users.noreply.github.com>
2024-08-14 23:44:55 -0700

    wwwwwww

--- src/Pages/Home Page/Right Side/HomePageRightSide.jsx
+++ src/Pages/Home Page/Right Side/HomePageRightSide.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './homePageRightSide.css'
 import nskLogo from '../../../assets/nsklogo.png'
 import TextField from '@mui/material/TextField';
+import { Button } from '@mui/material';
+import { BsGoogle } from 'react-icons/bs';
+import { FaGoogle } from 'react-icons/fa';
+import { GrGoogle, GrGooglePlay } from 'react-icons/gr';
+import { ImGoogle } from 'react-icons/im';
 
 const HomePageRightSide = () => {
   return (
@@ -26,7 +31,12 @@ const HomePageRightSide = () => {
      <div className='btnFields'>
 
       <button className='fgtPasswordBtn'>Forgot Password?</button>
-      <button className='sbmtBtn'>SUBMIT</button>
+      <button className='sbmtBtn'>Submit</button>
+     </div>
+
+     <div >
+      <ImGoogle/>
+      <Button className="googleBtn">Continue with Google</Button>
      </div>
     </div>
   )
